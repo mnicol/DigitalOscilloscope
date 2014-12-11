b5a35187da420f9de80f4807fd88710361e08118
tdvoss <tdvoss@wisc.edu>
2014-12-10 19:40:12 -0600

    misc
    meow

--- DSO_dig_tb.v
+++ DSO_dig_tb.v
@@ -56,7 +56,7 @@ AFE_A2D iAFE(.clk(clk),.rst_n(rst_n),.adc_clk(adc_clk),.ch1_ss_n(ch1_ss_n),.ch2_
 //UART_comm_mstr iMSTR(.clk(clk), .rst_n(rst_n), .RX(TX), .TX(RX), .cmd(cmd_snd), .send_cmd(send_cmd),
 //                     .cmd_sent(cmd_sent), .resp_rdy(resp_rdy), .resp(resp_rcv), .clr_resp_rdy(clr_resp_rdy));
 UART_comm iUARTMSTR(.clk(clk), .rst_n(rst_n), .tx_data(tx_data), .trmt(trmt), .TX(RX), .tx_done(tx_done), .RX(TX),
-							.clr_cmd_rdy(clr_resp_rdy), .cmd_rdy(resp_rdy));
+							.clr_cmd_rdy(clr_resp_rdy), .cmd_rdy(resp_rdy), .cmd());
 
 
 /////////////////////////////////////
